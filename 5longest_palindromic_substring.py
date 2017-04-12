a6b489b92bdea4730f7f668e826152c3e5fe5ed6
Sasori <345563880@qq.com>
2017-04-12 16:08:02 +0800

    Create 5longest_palindromic_substring.py

---
+++ 5longest_palindromic_substring.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) == 1:
+            return s
+
+        max_s = ''
+        for idx, val in enumerate(s):
+            for i in [1, 2]:  # 奇偶对称判断
+                step = 0
+                while idx - step >= 0 and idx + (i - 1) + step < len(s):
+                    last_s = s[idx - step]
+                    next_s = s[idx + (i - 1) + step]
+                    if last_s == next_s:
+                        max_len = step * 2 + i
+                        if max_len > len(max_s):
+                            max_s = s[(idx - step):(idx + step + i)]
+                        step += 1
+                    else:
+                        break
+        return max_s
